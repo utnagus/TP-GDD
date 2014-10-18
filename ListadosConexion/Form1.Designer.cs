ae1868698bc716b2fe32653b0fe7da0c131edf19
jonathan <gastrelljonathan@gmail.com>
2014-10-18 18:21:50 -0300

    agregre carpeta con archivos donde hay conexion a db

---
+++ ListadosConexion/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Clinica_Frba.NewFolder9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(572, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "cargame los datos papa!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Enero-Junio",
+            "Julio-Diciembre"});
+            this.comboBox1.Location = new System.Drawing.Point(590, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(365, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Elija el semestre...";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Top 5 de las especialidades que más se registraron cancelaciones, tanto de pacien" +
+                "tes como de profesionales",
+            "Top 5 de la cantidad total de bonos farmacia vencidos por afiliado",
+            "Top 5 de las especialidades de médicos con más bonos de farmacia recetados",
+            "Top 10 de los afiliados que utilizaron bonos que ellos mismos no compraron"});
+            this.comboBox2.Location = new System.Drawing.Point(590, 61);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(365, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = "Elija una opción...";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 294);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ ListadosConexion/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Clinica_Frba.NewFolder9
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexion = new SqlConnection("Data Source=USUARIO-PC\\SQLSERVER2008;Initial Catalog=GD2C2013;Persist Security Info=True;User ID=sa;Password=gestiondedatos");
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            switch (comboBox2.SelectedIndex)
+            { 
+                case 0:
+                    string query = String.Format("select distinct top 5 COUNT(te.Nombre) Cantidad,te.Nombre  from ASDASD.Tipos_especialidades te join ASDASD.Especialidades_Medicas em on te.Tipo_Especialidad_ID=em.Tipo_Especialidad_ID join ASDASD.Profesionales_Especialidades pe on em.Especialidad_ID=pe.Especialidad_ID join ASDASD.Agendas a on pe.Profesional_ID=a.Profesional_ID join ASDASD.Turnos t on a.Agenda_ID=t.Agenda_ID join ASDASD.Turnos_Cancelados tc on t.Turno_ID=tc.Turno_ID group by te.Nombre order by Cantidad desc");
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand(query, conexion);
+                    SqlDataAdapter adapt = new SqlDataAdapter();
+                    adapt.SelectCommand = cmd;
+                    DataTable tabla = new DataTable();
+                    adapt.Fill(tabla);
+                    BindingSource bsource = new BindingSource();
+
+                    bsource.DataSource = tabla;
+                    dataGridView1.DataSource = bsource;
+                    adapt.Update(tabla);
+
+                    conexion.Close();
+                    break;
+                case 1:
+                    string query2 = String.Format("select distinct top 5  p.Persona_ID, SUM(Cantidad) as TotalBonos from ASDASD.Personas p join ASDASD.Compras c on p.Persona_ID=c.Afiliado_ID join ASDASD.Bonos b on c.Compra_ID=b.Compra_ID and b.Fecha_Prescripcion is not null and b.Fecha_Prescripcion < CONVERT(varchar,GETDATE(),126) group by p.Persona_ID order by TotalBonos desc");
+                    conexion.Open();
+                    SqlCommand cmd2 = new SqlCommand(query2, conexion);
+                    SqlDataAdapter adapt2 = new SqlDataAdapter();
+                    adapt2.SelectCommand = cmd2;
+                    DataTable tabla2 = new DataTable();
+                    adapt2.Fill(tabla2);
+                    BindingSource bsource2 = new BindingSource();
+
+                    bsource2.DataSource = tabla2;
+                    dataGridView1.DataSource = bsource2;
+                    adapt2.Update(tabla2);
+
+                    conexion.Close();
+                    break;
+                case 2:
+                    string query3 = String.Format("select te.Nombre from ASDASD.Tipos_especialidades te join ASDASD.Especialidades_Medicas em on te.Tipo_Especialidad_ID=em.Tipo_Especialidad_ID join ASDASD.Profesionales_Especialidades pe on em.Especialidad_ID=pe.Especialidad_ID join ASDASD.Bonos b on pe.Profesional_ID=b.Profesional_ID join ASDASD.Bonos_Medicamentos bm on b.Bono_ID=bm.Bono_Farmacia_ID order by te.Nombre");
+                    conexion.Open();
+                    SqlCommand cmd3 = new SqlCommand(query3, conexion);
+                    SqlDataAdapter adapt3 = new SqlDataAdapter();
+                    adapt3.SelectCommand = cmd3;
+                    DataTable tabla3 = new DataTable();
+                    adapt3.Fill(tabla3);
+                    BindingSource bsource3 = new BindingSource();
+
+                    bsource3.DataSource = tabla3;
+                    dataGridView1.DataSource = bsource3;
+                    adapt3.Update(tabla3);
+
+                    conexion.Close();
+                    break;
+                case 3:
+                    string query4 = String.Format("select top 10 p.Nro_Afiliado from ASDASD.Personas p join ASDASD.Bonos b on p.Nro_Afiliado=b.Afiliado_ID join ASDASD.Compras c on b.Compra_ID=c.Compra_ID where p.Nro_Afiliado <> c.Afiliado_ID");
+                    conexion.Open();
+                    SqlCommand cmd4 = new SqlCommand(query4, conexion);
+                    SqlDataAdapter adapt4 = new SqlDataAdapter();
+                    adapt4.SelectCommand = cmd4;
+                    DataTable tabla4 = new DataTable();
+                    adapt4.Fill(tabla4);
+                    BindingSource bsource4 = new BindingSource();
+
+                    bsource4.DataSource = tabla4;
+                    dataGridView1.DataSource = bsource4;
+                    adapt4.Update(tabla4);
+
+                    conexion.Close();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+ 
+        }
+    }
+}
